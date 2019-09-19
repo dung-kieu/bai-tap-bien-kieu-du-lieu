8c1ad0d8122243a530725c0428d3547ffb3003df
dung-kieu <Velvet99>
2019-09-19 16:20:15 +0700

    nhap diem 3 mon roi in ra mh diem tb va tong 3 mon cách khác

--- 12.js
+++ 12.js
@@ -3,5 +3,5 @@ let b = parseFloat(prompt("nhap diem mon hoa hoc"));
 let c = parseFloat(prompt("nhap diem mon sinh hoc"));
 d =(a+b+c)/3;
 e =(a+b+c);
-alert("diem tb la:  " +d);
-alert("tong ba mon la:  " + e);
+document.write("tb la : " + d);
+document.write("<br>tong la : " + e);
