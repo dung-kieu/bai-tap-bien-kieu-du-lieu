7e777e4f584d0903f00b54790f0f49faeb5bed14
dung-kieu <Velvet99>
2019-09-19 16:05:19 +0700

    nhap diem 3 mon roi in ra mh diem tb va tong 3 mon

---
+++ 12.js
@@ -0,0 +1,7 @@
+let a = parseFloat(prompt("nhap diem mon vat ly"));
+let b = parseFloat(prompt("nhap diem mon hoa hoc"));
+let c = parseFloat(prompt("nhap diem mon sinh hoc"));
+d =(a+b+c)/3;
+e =(a+b+c);
+alert("diem tb la:  " +d);
+alert("tong ba mon la:  " + e);
